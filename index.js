ca0a7ddee204632ec9045fc7013e73f89c7185cb
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-09 22:18:56 +0545

    I HATE THIS

--- index.js
+++ index.js
@@ -13,23 +13,23 @@ const app = express();
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-// app.get("/", function (req, res) {
-//     res.render("home");
-// });
-// app.get("/service", function (req, res) {
-//     res.render("service");
-// });
-// app.get("/contact", function (req, res) {
-//     res.render("contact");
-// });
-// app.get("/:id", function (req, res) {
-//     try {
-//         var id = Number(req.params.id);
-//         res.render('singlecar', { data: carData[id] });
-//     }catch(e){
+app.get("/", function (req, res) {
+    res.render("home");
+});
+app.get("/service", function (req, res) {
+    res.render("service");
+});
+app.get("/contact", function (req, res) {
+    res.render("contact");
+});
+app.get("/:id", function (req, res) {
+    try {
+        var id = Number(req.params.id);
+        res.render('singlecar', { data: carData[id] });
+    }catch(e){
         
-//     }
-// });
+    }
+});
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server started");
