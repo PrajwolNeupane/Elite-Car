662ad13a50ae649f63783e909346dbde8636b8d5
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-09 22:13:10 +0545

    It might work

--- index.js
+++ index.js
@@ -7,26 +7,29 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 const app = express();
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+// app.set('view engine', 'ejs')
+// app.use(express.static(__dirname + '/public'));
 
-app.get("/", function (req, res) {
-    res.render("home");
-});
-app.get("/service", function (req, res) {
-    res.render("service");
-});
-app.get("/contact", function (req, res) {
-    res.render("contact");
-});
-app.get("/:id", function (req, res) {
-    try {
-        var id = Number(req.params.id);
-        res.render('singlecar', { data: carData[id] });
-    }catch(e){
+// app.get("/", function (req, res) {
+//     res.render("home");
+// });
+// app.get("/service", function (req, res) {
+//     res.render("service");
+// });
+// app.get("/contact", function (req, res) {
+//     res.render("contact");
+// });
+// app.get("/:id", function (req, res) {
+//     try {
+//         var id = Number(req.params.id);
+//         res.render('singlecar', { data: carData[id] });
+//     }catch(e){
         
-    }
-});
+//     }
+// });
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server started");
